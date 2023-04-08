de869bd8aca6b352e65ac72a4352091c099dcf49
rainefpp <70712013+rainefpp@users.noreply.github.com>
2023-04-07 23:58:25 -0400

    update bd

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigInteger('rut')->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->integer('tipo_de_cuenta')->nullable();
             $table->timestamps();
         });
     }
