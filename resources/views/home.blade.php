36cf959bbae3098281dbfa5cd905a82e5e03ad1c
Nicoperez19 <nperez@ing.ucsc.cl>
2023-04-11 17:37:47 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.master')
 @include('navbar.header')
-@section('content')
 @include('sidebar.form_sidebar')
-<link rel="stylesheet" href="{{URL::to('assets/css/profile.css')}}">
-<main class="col bg-faded py-3 flex-grow-1">
-  
-</main>
+@section('content')
+   
+    <link rel="stylesheet" href="{{ URL::to('assets/css/profile.css') }}">
+    <main class="col bg-faded py-3 flex-grow-1">
+
+    </main>
 @endsection
