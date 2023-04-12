94a2c31c3cbe044c157fdfda264333326fbdfee7
rainefpp <70712013+rainefpp@users.noreply.github.com>
2023-04-11 22:01:28 -0400

    id rut
    Permite que el id clave primaria pueda ser un bigInteger (rut)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,14 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-<<<<<<< Updated upstream
-            $table->bigInteger('id')->primary()->unsigned();
-            $table->string('name');
-=======
-            $table->id();
+            $table->bigInteger('id')->primary()->unsigned();            
             $table->string('name_register');
             $table->string('last_name_register');
->>>>>>> Stashed changes
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
