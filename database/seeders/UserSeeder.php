d3795a090eb4aaf00debdfbff9d07054f5eea263
m-madariaga <121448640+m-madariaga@users.noreply.github.com>
2023-04-13 16:55:11 -0400

    Add files via upload

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -14,12 +15,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $role1 = Role::create(['name' => 'admin']);
+        $role2 = Role::create(['name' => 'analista']);
+        $role3 = Role::create(['name' => 'trabajador']);
+
         $user = new User();
             $user->name             = 'admin demo';
             $user->email            = 'admin@test.cl';
             $user->password         = bcrypt('asdf1234');
             $user->tipo_de_cuenta   = 1;
         $user->save();
+        $user->assignRole($role1);
 
         $user = new User();
             $user->name             = 'analista demo';
@@ -27,6 +33,7 @@ class UserSeeder extends Seeder
             $user->password         = bcrypt('asdf1234');
             $user->tipo_de_cuenta   = 2;
         $user->save();
+        $user->assignRole($role2);
 
         $user = new User();
             $user->name             = 'trabajador demo';
@@ -34,6 +41,7 @@ class UserSeeder extends Seeder
             $user->password         = bcrypt('asdf1234');
             $user->tipo_de_cuenta   = 3;
         $user->save();
+        $user->assignRole($role3);
         
     }
 }
