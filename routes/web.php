382bec4ed44b00278a12c5b82f6a5bce140bc9c1
m-madariaga <121448640+m-madariaga@users.noreply.github.com>
2023-04-13 16:51:03 -0400

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',function(){
+    return view('admin_welcome');
+})->name('/')->middleware('auth');
+
+Route::get('/analista',function(){
+    return view('analista_welcome');
+})->name('analista')->middleware('auth');
+
+Route::get('/trabajador',function(){
+    return view('trabajador_welcome');
+})->name('trabajador')->middleware('auth');
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
