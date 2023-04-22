d769860221bccda32fb51dd342046da255623438
Nicoperez19 <nperez@ing.ucsc.cl>
2023-04-22 01:05:11 -0400

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -13,42 +13,53 @@
                     <b>Dashboard</b>
                 </a>
             </li>
-            
             <li>
                 <a href="#userSubmenu" data-toggle="collapse" aria-expanded="false">
                     <i class="fas fa-user fa-lg" aria-hidden="true"></i>
                     <b>Usuarios</b>
                 </a>
                 <ul class="collapse list-unstyled" id="userSubmenu">
-                    <li><a href="{{ route('users.index') }}">Lista Usuarios</a></li>
+                    <li><a href="{{ route('users.index') }}">Listar Usuarios</a></li>
                     <li><a href="#">Roles</a></li>
                     <li><a href="#">Permisos</a></li>
                 </ul>
             </li>
            
             <li>
-                <a href="#">
+                <a href="#generatorSubmenu" data-toggle="collapse" aria-expanded="false">
+
                     <i class="fas fa-industry fa-lg" aria-hidden="true"></i>
                     <b>Generadores</b>
                 </a>
-                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false">
+                <ul class="collapse list-unstyled" id="generatorSubmenu">
+                    <li><a href="#">Listar generadores</a></li>
+                    <li><a href="#">Gráficos</a></li>
+                    <li><a href="#">Envío de datos</a></li>
+                    <li><a href="#">Mantenciones</a></li>
+                </ul>
+
+                <a href="#boilerSubmenu" data-toggle="collapse" aria-expanded="false">
                     <i class="fas fa-fire fa-lg" aria-hidden="true"></i>
                     <b>Calderas</b>
                 </a>
-                <ul class="collapse list-unstyled" id="pageSubmenu">
-                    <li><a href="#">Pagina</a></li>
+                <ul class="collapse list-unstyled" id="boilerSubmenu">
+                    <li><a href="#">Listar calderas</a></li>
+                    <li><a href="#">Gráficos</a></li>
+                    <li><a href="#">Mantenciones</a></li>
                 </ul>
             </li>
-            
             <li>
-                <a href="#">
+                <a href="#reportSubmenu" data-toggle="collapse" aria-expanded="false">
                     <i class="fas fa-chart-bar fa-lg" aria-hidden="true"></i>
                     <b>Reporte</b>
                 </a>
+                <ul class="collapse list-unstyled" id="reportSubmenu">
+                    <li><a href="#">Generadores</a></li>
+                    <li><a href="#">Caldera</a></li>
+                </ul>
             </li>
-            
-            <li>
 
+            <li>
                 <a href="#">
                     <i class="fa fa-book fa-lg" aria-hidden="true"></i>
                     <b>Sección de API´S</b>
