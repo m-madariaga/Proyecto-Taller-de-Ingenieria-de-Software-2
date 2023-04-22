ae71d42ab3894e5e9c174217d902831ffe81e22c
derkiab <110829988+derkiab@users.noreply.github.com>
2023-04-22 16:06:43 -0400

    update migration, user model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('rut')->nullable();
+
             $table->string('name');
-            $table->unsignedBigInteger('run')->nullable();
+            $table->unsignedBigInteger('run');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
