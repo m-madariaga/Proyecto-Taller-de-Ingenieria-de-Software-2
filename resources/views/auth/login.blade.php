983152690e5abfa687638cfa2afbe0b84d662b05
Nicoperez19 <nperez@ing.ucsc.cl>
2023-04-23 20:29:17 -0400

    Agregando diseño al login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,72 +1,92 @@
 @extends('layouts.app')
 
-<head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
-</head>
 @section('login')
-<div class="container">
-    <div class="d-flex justify-content-center h-100">
-        <div class="card">
-            <div class="card-header">
-                <h3>Inicio Sesión</h3>
+    <div id="login" class="container-fluid mt-0">
+        <div id="page-content" class="row">
+            <div id="region-main-box" class="col-12">
+                <section id="region-main" class="col-12">
+                    <div role="main">
+                        <div class="my-1 my-sm-5"></div>
 
-            </div>
-            <div class="card-body">
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
+                        <div class="row justify-content-center logo">
+                            <img src="{{ asset('images/logo.png') }}"
+                                title="Landes" alt="Landes" id="logo">
+                        </div>
+                        <div class="row justify-content-center my-4">
+                            <div class="col-12 col-md-12 col-lg-8">
+                                <div class="card">
 
 
-                    <div class="row mb-3">
-                        <div class="input-group form-group col-md-6">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-user" style="color: #ffffff;"></i></span>
+                                    <div class="card-header">
+                                        <h3>Inicio Sesión</h3>
+                                    </div>
+
+                                    <div class="card-body">
+                                        <form method="POST" action="{{ route('login') }}">
+                                            @csrf
+
+                                            <div class="row mb-3">
+                                                <div class="input-group form-group col-md-6">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"><i class="fas fa-user"
+                                                                style="color: #ffffff;"></i></span>
+                                                    </div>
+                                                    <input id="email" type="email" placeholder="Correo Electrónico"
+                                                        class="form-control @error('email') is-invalid @enderror"
+                                                        name="email" value="{{ old('email') }}" required
+                                                        autocomplete="email" autofocus>
+                                                    @error('email')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                            <div class="row mb-3">
+                                                <div class="input-group form-group col-md-6">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"><i class="fas fa-key "
+                                                                style="color: #ffffff;"></i></span>
+                                                    </div>
+                                                    <input id="password" type="password" placeholder="Contraseña"
+                                                        class="form-control @error('password') is-invalid @enderror"
+                                                        name="password" required autocomplete="current-password">
+
+                                                </div>
+                                                <div class="row align-items-center remember col-md-6">
+                                                    <input type="checkbox">Recordarme
+                                                </div>
+
+                                            </div>
+
+
+
+                                            <div class="form-group">
+                                                <div class="col-md-8 offset-md-4">
+                                                    <button type="submit" class="btn btn-primary">
+                                                        {{ __('Ingreso') }}
+                                                    </button>
+
+
+                                                </div>
+                                            </div>
+
+                                            <div class="card-footer">
+                                                <div class="d-flex justify-content-center">
+                                                    @if (Route::has('password.request'))
+                                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                            {{ __('¿Olvidaste tu contraseña?') }}
+                                                        </a>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
-                            <input id="email" type="email" placeholder="Correo Electrónico" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                            @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
                         </div>
                     </div>
-
-                    <div class="row mb-3">
-                        <div class="input-group form-group col-md-6">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fas fa-key " style="color: #ffffff;"></i></span>
-                            </div>
-                            <input id="password" type="password" placeholder="Contraseña" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                        </div>
-                        <div class="row align-items-center remember col-md-6">
-                            <input type="checkbox">Recordarme
-                        </div>
-
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-md-8 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
-                                {{ __('Ingreso') }}
-                            </button>
-
-
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="card-footer">
-                <div class="d-flex justify-content-center">
-                    @if (Route::has('password.request'))
-                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                        {{ __('¿Olvidaste tu contraseña?') }}
-                    </a>
-                    @endif
-                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
